cd09b28e1cfaaea759f6f6ce338421e0a46e2f4e
jholder7 <jholder7@murraystate.edu>
2023-10-12 13:38:54 -0500

    basic crub operations

--- Express-App/App.js
+++ Express-App/App.js
@@ -7,6 +7,26 @@ app.get('/',(req, res) => {
     res.send('Hello, Express.js');
 });
 
+app.get('/api/v1/tasks/getTasks', (req, res) => {
+
+});
+
+app.get('/api/v1/tasks/getTaskByID', (req, res) => {
+
+});
+
+app.get('/api/v1/tasks/createTask', (req, res) => {
+
+});
+
+app.get('/api/v1/tasks/updateTaskByID', (req, res) => {
+
+});
+
+app.get('/api/v1/tasks/deleteTaskByID', (req, res) => {
+
+});
+
 //Server
 app.listen(port, () => {
     console.log('Server is up and running on port 3000');

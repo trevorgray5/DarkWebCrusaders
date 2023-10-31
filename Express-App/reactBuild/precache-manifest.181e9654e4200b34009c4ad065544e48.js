ef80a27124dc88b06980c4d848466dec95a82f52
jholder7 <jholder7@murraystate.edu>
2023-10-31 13:26:53 -0500

    fix

---
+++ Express-App/reactBuild/precache-manifest.181e9654e4200b34009c4ad065544e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4c82cac7217d1d8ce4dafee079c7d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bcf52f1cc370ac3cccb",
+    "url": "./static/css/main.ea8e9686.chunk.css"
+  },
+  {
+    "revision": "37e868807e658c39bdf8",
+    "url": "./static/js/2.b82fe647.chunk.js"
+  },
+  {
+    "revision": "278bb1a1e59d10282b78",
+    "url": "./static/js/3.3f8aa67b.chunk.js"
+  },
+  {
+    "revision": "4bcf52f1cc370ac3cccb",
+    "url": "./static/js/main.40b01a41.chunk.js"
+  },
+  {
+    "revision": "e0bb8bcff58e7f492004",
+    "url": "./static/js/runtime~main.8a516337.js"
+  }
+]);

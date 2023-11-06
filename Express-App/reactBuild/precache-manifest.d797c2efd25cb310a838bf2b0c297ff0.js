2f76f9f95a663336e2ae0ec478aedf399073923f
Justin Holder <Jholder7@murraystate.edu>
2023-11-05 19:44:45 -0600

    Added new react build to express

---
+++ Express-App/reactBuild/precache-manifest.d797c2efd25cb310a838bf2b0c297ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9875e6c672b325c647fa76a0e1fed8a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac2503359fe0a58ee9ce",
+    "url": "./static/css/main.ad36b33e.chunk.css"
+  },
+  {
+    "revision": "dedd7872b33e74c659b6",
+    "url": "./static/js/2.c662a7ee.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "./static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "ac2503359fe0a58ee9ce",
+    "url": "./static/js/main.3a344187.chunk.js"
+  },
+  {
+    "revision": "a736e98ceb18a5c22f8e",
+    "url": "./static/js/runtime~main.dff00fec.js"
+  }
+]);

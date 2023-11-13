6d73a305dbbd2d44388c4c0bbf4641fa78410956
Justin Holder <Jholder7@murraystate.edu>
2023-11-12 18:15:47 -0600

    Added my a few unit tests and fixed a small issue with taskcards being unable to be tested due to render order.

--- my-first-app/src/App.test.js
+++ my-first-app/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -7,6 +7,7 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
 //Add Button click Test
 describe('AddButton component', () => {
   it('Button click triggers click event', () => {
@@ -17,6 +18,40 @@ describe('AddButton component', () => {
     addButton.onclick = mockOnClick;
     // Act
     fireEvent.click(addButton);
+
+    // Assert
+    expect(mockOnClick).toHaveBeenCalled();
+  });
+});
+
+describe('DeleteButton Component', () => {
+  it('Delete click triggers click event', async () => {
+    // Arrange
+    const mockOnClick = jest.fn();
+    render(<App/>);
+    //wait for the components to be rendered from api
+    let deleteButton = screen.getByText("Delete");
+    deleteButton.onclick = mockOnClick;
+
+    // Act
+    fireEvent.click(deleteButton);
+
+    // Assert
+    expect(mockOnClick).toHaveBeenCalled();
+  });
+});
+
+describe('CompleteButton component', () => {
+  it('Complete click triggers click even', async () => {
+    // Arrange
+    const mockOnClick = jest.fn();
+    render(<App/>);
+    //wait for the components to be rendered from api
+    let CompleteButton = screen.getByText("Complete");
+    CompleteButton.onclick = mockOnClick;
+
+    // Act
+    fireEvent.click(CompleteButton);
 
     // Assert
     expect(mockOnClick).toHaveBeenCalled();
